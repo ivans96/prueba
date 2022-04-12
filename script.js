fbf44f2c3f6d99827cbe95b437a061d5e03e8514
ivans96 <ivansanchez091@gmail.com>
2022-04-12 03:04:21 -0300

    holi

---
+++ script.js
@@ -0,0 +1,38 @@
+/*var css = document.querySelector("h3");
+var color1= document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body= document.getElementById("gradient");
+
+function linearGradient(){
+	body.style.background = 
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+color2.value
+	+ ")";
+	
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", linearGradient);
+	
+	
+color2.addEventListener("input", linearGradient);
+*/
+
+/*const name= "Sally";
+const age= 35;
+const pet= "horse";
+
+ 
+const greetingBest= `salute ${name} and tu edad de ${age} y mascota ${pet}`;   */   
+
+/*function greet(name='', age=30, pet='cat') {
+	return `salute ${name} and tu edad de ${age} y mascota ${pet}`;
+}*/
+
+const comino = [1,2,10,16];
+
+const loop = comino.forEach((cominitos) =>{
+	console.log(cominitos);
+})
